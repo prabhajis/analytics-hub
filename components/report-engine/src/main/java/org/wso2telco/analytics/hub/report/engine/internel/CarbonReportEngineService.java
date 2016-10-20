4c66ebe2750d4c487d1766cf927fb6786f6d4c35
Tharik Kanaka <tharik.kanaka@gmail.com>
2016-10-20 12:26:11 +0530

    CSV Report generation osgi servive completed

---
+++ components/report-engine/src/main/java/org/wso2telco/analytics/hub/report/engine/internel/CarbonReportEngineService.java
@@ -0,0 +1,76 @@
+package org.wso2telco.analytics.hub.report.engine.internel;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.analytics.dataservice.commons.AnalyticsDataResponse;
+import org.wso2.carbon.analytics.dataservice.commons.SearchResultEntry;
+import org.wso2.carbon.analytics.dataservice.core.AnalyticsDataServiceUtils;
+import org.wso2.carbon.analytics.datasource.commons.Record;
+import org.wso2.carbon.analytics.datasource.commons.exception.AnalyticsException;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
+import org.wso2telco.analytics.hub.report.engine.ReportEngineService;
+import org.wso2telco.analytics.hub.report.engine.internel.ds.ReportEngineServiceHolder;
+import org.wso2telco.analytics.hub.report.engine.internel.util.CSVWriter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarbonReportEngineService implements ReportEngineService {
+
+    private static final Log log = LogFactory.getLog(CarbonReportEngineService.class);
+
+    public void generateCSVReport(String tableName, String query, String reportName, int maxLength) {
+        try {
+            int tenantId = PrivilegedCarbonContext.getThreadLocalCarbonContext().getTenantId(true);
+
+            int searchCount =  ReportEngineServiceHolder.getAnalyticsDataService()
+                                                .searchCount(tenantId, tableName, query);
+
+            int writeBufferLength = 8192;
+
+            //Check weather search count is greater than the max file length and split files accordingly
+            if (searchCount > maxLength) {
+                for (int i = 0; i < searchCount; ) {
+                    int end = i + maxLength;
+                    String filepath = reportName +"-" + ((i / maxLength) + 1) + ".csv";
+                    generateCSV(tableName, query, filepath, tenantId, i, end, writeBufferLength);
+                    i = end;
+                }
+            } else {
+                String filepath = reportName + ".csv";
+                generateCSV(tableName, query, filepath, tenantId, 0, searchCount, writeBufferLength);
+            }
+
+
+        } catch (AnalyticsException e) {
+            log.error("Data cannot be loaded for " + reportName + "report", e);
+        }
+
+
+    }
+
+    public void generateCSV(String tableName, String query, String filePath, int tenantId, int start,
+                            int maxLength, int writeBufferLength)
+            throws AnalyticsException{
+
+        List<SearchResultEntry> resultEntries = ReportEngineServiceHolder.getAnalyticsDataService()
+                .search(tenantId, tableName, query, start, maxLength);
+
+        List<String> ids = new ArrayList<>();
+        for (SearchResultEntry entry : resultEntries) {
+            ids.add(entry.getId());
+        }
+        AnalyticsDataResponse resp = ReportEngineServiceHolder.getAnalyticsDataService()
+                .get(tenantId, tableName, 1, null, ids);
+
+        List<Record> records = AnalyticsDataServiceUtils
+                .listRecords(ReportEngineServiceHolder.getAnalyticsDataService(), resp);
+
+        try {
+            CSVWriter.write(records, writeBufferLength, filePath);
+        } catch (IOException e) {
+            log.error("CSV file " + filePath + " cannot be created", e);
+        }
+    }
+}
---
+++ components/report-engine/src/main/java/org/wso2telco/analytics/hub/report/engine/internel/util/CSVWriter.java
@@ -0,0 +1,59 @@
+/*
+ *  Copyright (c) 2016 WSO2.Telco Inc. (http://www.wso2telco.com) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package org.wso2telco.analytics.hub.report.engine.internel.util;
+
+import org.wso2.carbon.analytics.datasource.commons.Record;
+
+import java.io.*;
+import java.util.List;
+
+public class CSVWriter {
+
+    public static void write(List<Record> records, int bufSize, String filePath) throws IOException {
+
+        File file = new File(filePath);
+
+        FileWriter writer = new FileWriter(file);
+        BufferedWriter bufferedWriter = new BufferedWriter(writer, bufSize);
+
+
+        for (Record record: records) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(record.getValues().get("api"));
+            sb.append(',');
+            sb.append(record.getValues().get("msisdn"));
+            sb.append(',');
+            sb.append(record.getValues().get("responseTime"));
+            sb.append(',');
+            sb.append(record.getValues().get("serviceProvider"));
+            sb.append(',');
+            sb.append(record.getValues().get("applicationName"));
+            sb.append(',');
+            sb.append(record.getValues().get("operatorId"));
+            sb.append(',');
+            sb.append(record.getValues().get("responseCode"));
+
+            sb.append(System.getProperty("line.separator"));
+
+            bufferedWriter.write(sb.toString());
+        }
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
