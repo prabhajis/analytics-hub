e19c0f24456ec1e9e2f7d490c775a15312791690
roshan <roshan.saputhanthri@dialog.lk>
2017-05-18 15:26:33 +0530

    Charge Entities

---
+++ components/org.wso2telco.analytics.pricing/src/main/java/org/wso2telco/analytics/service/UsageTiers.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright  (c) 2015-2016, WSO2.Telco Inc. (http://www.wso2telco.com) All Rights Reserved.
+ *
+ * WSO2.Telco Inc. licences this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package org.wso2telco.analytics.service;
+
+public class UsageTiers{
+	private String rateId;
+	private String min;
+	private String max;
+        private String tierName;
+	
+	public String getRateId() {
+		return rateId;
+	}
+	public void setRateId(String rateId) {
+		this.rateId = rateId;
+	}
+	public String getMin() {
+		return min;
+	}
+	public void setMin(String min) {
+		this.min = min;
+	}
+	public String getMax() {
+		return max;
+	}
+	public void setMax(String max) {
+		this.max = max;
+	}
+
+    public String getTierName() {
+        return tierName;
+    }
+
+    public void setTierName(String tierName) {
+        this.tierName = tierName;
+    }
+        
+        
+        
+}
