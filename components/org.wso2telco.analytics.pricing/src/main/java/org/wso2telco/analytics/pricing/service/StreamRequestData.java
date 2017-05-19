79061283acf483bfea6870936e046115b60ca311
roshan <roshan.saputhanthri@dialog.lk>
2017-05-19 15:03:15 +0530

    Stream modified for das summary

--- components/org.wso2telco.analytics.pricing/src/main/java/org/wso2telco/analytics/pricing/service/StreamRequestData.java
+++ components/org.wso2telco.analytics.pricing/src/main/java/org/wso2telco/analytics/pricing/service/StreamRequestData.java
@@ -25,79 +25,32 @@ import java.sql.Date;
  */
 public class StreamRequestData {
 
-    private String RequestRawID;
     private String api;
-    private String api_version;
-    private String version;
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getSubcategory() {
-        return subcategory;
-    }
-
-    public void setSubcategory(String subcategory) {
-        this.subcategory = subcategory;
-    }
-
-    public String getMerchant() {
-        return merchant;
-    }
-
-    public void setMerchant(String merchant) {
-        this.merchant = merchant;
-    }
-    private String apiPublisher;
-    private String consumerKey;
     private String userId;
+    private Integer applicationid;
     private Integer response_count;
     private String requestId;
     private String operatorId;
-    private String msisdn;
     private String operatorRef;
-    private BigDecimal chargeAmount;
-    private String purchaseCategoryCode;
-    private Integer year;
-    private Integer month;
-    private Integer day;
+    private BigDecimal chargeAmount;    
     private Date reqtime;
     private String category;
     private String subcategory;
     private String merchant;
 
-    public StreamRequestData(String RequestRawID, String api, String api_version, String version, String apiPublisher, String consumerKey, String userId, Integer response_count, String requestId, String operatorId, String msisdn, String operatorRef, BigDecimal chargeAmount, String purchaseCategoryCode, Integer year, Integer month, Integer day, Date reqtime) {
-        this.RequestRawID = RequestRawID;
+    public StreamRequestData(String api, String userId, Integer applicationid, Integer response_count, String requestId, String operatorId, String operatorRef, BigDecimal chargeAmount, Date reqtime, String category, String subcategory, String merchant) {
         this.api = api;
-        this.api_version = api_version;
-        this.version = version;
-        this.apiPublisher = apiPublisher;
-        this.consumerKey = consumerKey;
         this.userId = userId;
+        this.applicationid = applicationid;
         this.response_count = response_count;
         this.requestId = requestId;
         this.operatorId = operatorId;
-        this.msisdn = msisdn;
         this.operatorRef = operatorRef;
         this.chargeAmount = chargeAmount;
-        this.purchaseCategoryCode = purchaseCategoryCode;
-        this.year = year;
-        this.month = month;
-        this.day = day;
         this.reqtime = reqtime;
-    }
-
-    public String getRequestRawID() {
-        return RequestRawID;
-    }
-
-    public void setRequestRawID(String RequestRawID) {
-        this.RequestRawID = RequestRawID;
+        this.category = category;
+        this.subcategory = subcategory;
+        this.merchant = merchant;
     }
 
     public String getApi() {
@@ -108,44 +61,20 @@ public class StreamRequestData {
         this.api = api;
     }
 
-    public String getApi_version() {
-        return api_version;
-    }
-
-    public void setApi_version(String api_version) {
-        this.api_version = api_version;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    public String getApiPublisher() {
-        return apiPublisher;
-    }
-
-    public void setApiPublisher(String apiPublisher) {
-        this.apiPublisher = apiPublisher;
-    }
-
-    public String getConsumerKey() {
-        return consumerKey;
-    }
-
-    public void setConsumerKey(String consumerKey) {
-        this.consumerKey = consumerKey;
-    }
-
     public String getUserId() {
         return userId;
     }
 
     public void setUserId(String userId) {
         this.userId = userId;
+    }
+
+    public Integer getApplicationid() {
+        return applicationid;
+    }
+
+    public void setApplicationid(Integer applicationid) {
+        this.applicationid = applicationid;
     }
 
     public Integer getResponse_count() {
@@ -172,14 +101,6 @@ public class StreamRequestData {
         this.operatorId = operatorId;
     }
 
-    public String getMsisdn() {
-        return msisdn;
-    }
-
-    public void setMsisdn(String msisdn) {
-        this.msisdn = msisdn;
-    }
-
     public String getOperatorRef() {
         return operatorRef;
     }
@@ -196,38 +117,6 @@ public class StreamRequestData {
         this.chargeAmount = chargeAmount;
     }
 
-    public String getPurchaseCategoryCode() {
-        return purchaseCategoryCode;
-    }
-
-    public void setPurchaseCategoryCode(String purchaseCategoryCode) {
-        this.purchaseCategoryCode = purchaseCategoryCode;
-    }
-
-    public Integer getYear() {
-        return year;
-    }
-
-    public void setYear(Integer year) {
-        this.year = year;
-    }
-
-    public Integer getMonth() {
-        return month;
-    }
-
-    public void setMonth(Integer month) {
-        this.month = month;
-    }
-
-    public Integer getDay() {
-        return day;
-    }
-
-    public void setDay(Integer day) {
-        this.day = day;
-    }
-
     public Date getReqtime() {
         return reqtime;
     }
@@ -235,7 +124,31 @@ public class StreamRequestData {
     public void setReqtime(Date reqtime) {
         this.reqtime = reqtime;
     }
-    
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSubcategory() {
+        return subcategory;
+    }
+
+    public void setSubcategory(String subcategory) {
+        this.subcategory = subcategory;
+    }
+
+    public String getMerchant() {
+        return merchant;
+    }
+
+    public void setMerchant(String merchant) {
+        this.merchant = merchant;
+    }
+
     
 
 }
