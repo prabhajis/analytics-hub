5f05c8231abb25bbe73c6efbcf5d33734e3ad6e1
nuwan <nuwancs@gmail.com>
2017-05-23 14:48:37 +0530

    siddhi to pricing integration, required parameters are passed to backend

--- components/report-engine/src/main/java/org/wso2telco/analytics/hub/report/engine/BillingStreamProcessor.java
+++ components/report-engine/src/main/java/org/wso2telco/analytics/hub/report/engine/BillingStreamProcessor.java
@@ -1,128 +1,118 @@
 package org.wso2telco.analytics.hub.report.engine;
 
 import org.wso2.siddhi.core.query.processor.stream.StreamProcessor;
+
 import java.util.List;
+import java.math.BigDecimal;
 import java.util.ArrayList;
-import org.wso2.siddhi.core.event.ComplexEvent;
+import java.sql.Date;
+import java.util.Map;
+
 import org.wso2.siddhi.core.event.ComplexEventChunk;
 import org.wso2.siddhi.core.event.stream.StreamEvent;
 
 import org.wso2.siddhi.core.query.processor.Processor;
 import org.wso2.siddhi.core.event.stream.StreamEventCloner;
 import org.wso2.siddhi.core.event.stream.populater.ComplexEventPopulater;
-// import org.wso2.siddhi.core.executor.ConstantExpressionExecutor;
-import org.wso2.siddhi.core.event.ComplexEventChunk;
 
 import org.wso2.siddhi.query.api.definition.Attribute;
 import org.wso2.siddhi.query.api.definition.AbstractDefinition;
 import org.wso2.siddhi.core.executor.ExpressionExecutor;
 import org.wso2.siddhi.core.config.ExecutionPlanContext;
-// import org.wso2.siddhi.query.api.exception.ExecutionPlanValidationException;
 
-// import net.minidev.json.JSONArray;
-// import net.minidev.json.JSONObject;
-// import net.minidev.json.parser.JSONParser;
-// import net.minidev.json.parser.ParseException;
+import org.wso2telco.analytics.pricing.service.*;
 
+import java.util.HashMap;
 
 public class BillingStreamProcessor extends StreamProcessor {
 
-    // private int paramCount = 0;                                         // Number of x variables +1
-    // private int calcInterval = 1;                                       // The frequency of regression calculation
-    // private int batchSize = 1000000000;                                 // Maximum # of events, used for regression calculation
-    // private double ci = 0.95;                                           // Confidence Interval
-    // private final int SIMPLE_LINREG_INPUT_PARAM_COUNT = 2;              // Number of Input parameters in a simple linear regression
-    // // private RegressionCalculator regressionCalculator = null;
-    // private int paramPosition = 0;
-
     protected List<Attribute> init(AbstractDefinition inputDefinition,
-                                   ExpressionExecutor[] attributeExpressionExecutors, ExecutionPlanContext executionPlanContext) {
-
-        // if (attributeExpressionExecutors[1] instanceof ConstantExpressionExecutor) {
-        //     jsonConstantExecutorForFunction = (ConstantExpressionExecutor) attributeExpressionExecutors[1];
-        // } else {
-        //     throw new ExecutionPlanValidationException(
-        //             "This should have a constant parameter as the 2nd attribute but doesn't found a constant "
-        //                     + attributeExpressionExecutors[1].getClass().getCanonicalName());
-        // }
-
-        // if (attributeExpressionExecutors[0] instanceof VariableExpressionExecutor) {
-        //     jsonVariableExecutor = (VariableExpressionExecutor) attributeExpressionExecutors[0];
-        // } else {
-        //     throw new ExecutionPlanValidationException(
-        //             "This should have a variable expression as the 1st attribute but doesn't found a variable expression "
-        //                     + attributeExpressionExecutors[0].getClass().getCanonicalName());
-        // }
+                                   ExpressionExecutor[] attributeExpressionExecutors, ExecutionPlanContext
+                                           executionPlanContext) {
 
         return new ArrayList<Attribute>();
     }
 
-        @Override
-    protected void process(ComplexEventChunk<StreamEvent> streamEventChunk, Processor nextProcessor, StreamEventCloner streamEventCloner, ComplexEventPopulater complexEventPopulater) {
-  log.error("IN.........................")     ;
+    @Override
+    protected void process(ComplexEventChunk<StreamEvent> streamEventChunk, Processor nextProcessor,
+                           StreamEventCloner streamEventCloner, ComplexEventPopulater complexEventPopulater) {
         List<ComplexEventChunk<StreamEvent>> complexEventChunkList = new ArrayList<ComplexEventChunk<StreamEvent>>();
-synchronized (this) {
-        while (streamEventChunk.hasNext()) {
-            
+        synchronized (this) {
+            while (streamEventChunk.hasNext()) {
 
-                                // Creates a new complex event chunk
-                    ComplexEventChunk<StreamEvent> newComplexEventChunk = new ComplexEventChunk<StreamEvent>(true);
+                // Creates a new complex event chunk
+                ComplexEventChunk<StreamEvent> newComplexEventChunk = new ComplexEventChunk<StreamEvent>(true);
 
-                    StreamEvent compressedEvent = (StreamEvent) streamEventChunk.next();
-                    Object[] parameterSet = compressedEvent.getOutputData();
+                StreamEvent compressedEvent = (StreamEvent) streamEventChunk.next();
+                Object[] parameterSet = compressedEvent.getOutputData();
 
-                    private String api;
-                    private String userId;
-                    private Integer applicationid;
-                    private Integer response_count;
-                    private String requestId;
-                    private String operatorId;
-                    private String operatorRef;
-                    private BigDecimal chargeAmount;    
-                    private Date reqtime;
-                    private String category;
-                    private String subcategory;
-                    private String merchant;
-
-parameterSet[5]="nuwan";
-
-if (parameterSet[1] == null){
-    parameterSet[1] = 0.0;
-}
-
-if (parameterSet[2] == null){
-    parameterSet[2] = 0.0;
-}
-
-double count = (Double)parameterSet[1];
-            parameterSet[1] = count+1;
-
-        //     Object[] inputData = new Object[attributeExpressionLength-paramPosition];
-        //     for (int i = paramPosition; i < attributeExpressionLength; i++) {
-        //         inputData[i-paramPosition] = attributeExpressionExecutors[i].execute(complexEvent);
-        //     }
-        //     Object[] outputData = regressionCalculator.calculateLinearRegression(inputData);
- 
-        //     // Skip processing if user has specified calculation interval
-        //     if (outputData == null) {
-        //         streamEventChunk.remove();
-        //     } else {
-        //         complexEventPopulater.populateComplexEvent(complexEvent, outputData);
-        //     }
-             addToComplexEventChunk(complexEventPopulater, newComplexEventChunk, parameterSet);
-             complexEventChunkList.add(newComplexEventChunk);
-
-        }
-}
-                    if (complexEventChunkList.size() > 0) {
-                for (ComplexEventChunk<StreamEvent> complexEventChunk : complexEventChunkList) {
-                    nextProcessor.process(complexEventChunk);
+                if (parameterSet[0] == null) {
+                    parameterSet[0] = 0;
                 }
+
+                if (parameterSet[1] == null) {
+                    parameterSet[1] = 0.0;
+                }
+
+                String direction = parameterSet[4].toString();
+                if (direction.equals("sb")) {
+
+                    String api = parameterSet[5].toString();
+                    Integer applicationid = (Integer) parameterSet[8];
+                    Integer response_count = (Integer) parameterSet[19];
+                    String requestId = parameterSet[2].toString();
+                    String operatorId = parameterSet[12].toString();
+                    String operatorRef = parameterSet[14].toString();
+                    BigDecimal chargeAmount = (BigDecimal) parameterSet[15];
+                    Date reqtime = new Date((long) parameterSet[3]);
+                    String category = parameterSet[16].toString();
+                    String subcategory = parameterSet[17].toString();
+                    String merchant = parameterSet[18].toString();
+                    String operation = parameterSet[13].toString();
+                    String serviceProvider = parameterSet[9].toString();
+
+                    StreamRequestData streamRequestData = new StreamRequestData(
+                            api, serviceProvider, applicationid,
+                            response_count, requestId, operatorId,
+                            operatorRef, chargeAmount, reqtime,
+                            category, subcategory, merchant);
+
+                    int count = (int) parameterSet[0];
+
+                    Map<CategoryCharge, BilledCharge> apiCount = new HashMap<CategoryCharge, BilledCharge>();
+
+                    BilledCharge billcharge = new BilledCharge(count);
+                    CategoryCharge categoryCharge = new CategoryCharge(200, category, subcategory);
+                    apiCount.put(categoryCharge, billcharge);
+                }
+
+//temp code
+                int count = (int) parameterSet[0];
+                parameterSet[0] = count + 1;
+
+                double total = (Double) parameterSet[1];
+                parameterSet[1] = total + 150;
+//end of temp code
+
+                parameterSet[20] = "RC-112015";//rate card
+                parameterSet[21] = 60;//opCommision
+                parameterSet[22] = 10;//spCommision
+                parameterSet[23] = 30;//hbCommision
+
+                addToComplexEventChunk(complexEventPopulater, newComplexEventChunk, parameterSet);
+                complexEventChunkList.add(newComplexEventChunk);
+
             }
- 
+        }
+        if (complexEventChunkList.size() > 0) {
+            for (ComplexEventChunk<StreamEvent> complexEventChunk : complexEventChunkList) {
+                nextProcessor.process(complexEventChunk);
+            }
+        }
+
     }
 
-        /**
+    /**
      * Add to complex event chunk
      *
      * @param complexEventPopulater complexEventPopulater
@@ -137,14 +127,17 @@ double count = (Double)parameterSet[1];
         newComplexEventChunk.add(newStreamEvent);
     }
 
-    public void start() {    }
+    public void start() {
+    }
 
-    public void stop() {    }
+    public void stop() {
+    }
 
     public Object[] currentState() {
         return new Object[0];
     }
 
-    public void restoreState(Object[] state) {    }
-  
+    public void restoreState(Object[] state) {
+    }
+
 }
