7ec4bd8c9827e69feaa7b58328fa9db14049b535
Nuwansenanayake <nuwancs@gmail.com>
2017-05-25 09:37:49 +0000

    Update BillingStreamProcessor.java

--- components/report-engine/src/main/java/org/wso2telco/analytics/hub/report/engine/BillingStreamProcessor.java
+++ components/report-engine/src/main/java/org/wso2telco/analytics/hub/report/engine/BillingStreamProcessor.java
@@ -81,7 +81,7 @@ public class BillingStreamProcessor extends StreamProcessor {
                             api, serviceProvider, applicationid,
                             response_count, requestId, operatorId,
                             operatorRef, chargeAmount, reqtime,
-                            category, subcategory, merchant,200);
+                            category, subcategory, merchant);
 
                     int count = (int) parameterSet[0];
 
