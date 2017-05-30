f89214664e2317881120d0e67bf7156ec9952209
nuwan <nuwancs@gmail.com>
2017-05-30 10:47:03 +0530

    new columns added for export stream to support tax total

--- components/report-engine/src/main/java/org/wso2telco/analytics/hub/report/engine/BillingStreamProcessor.java
+++ components/report-engine/src/main/java/org/wso2telco/analytics/hub/report/engine/BillingStreamProcessor.java
@@ -76,7 +76,9 @@ public class BillingStreamProcessor extends StreamProcessor {
                             operatorRef, chargeAmount, reqtime,
                             category, subcategory, merchant, operation);
 
-                    int count = (int) parameterSet[0];
+                    streamRequestData.setStatus((int)parameterSet[34]);
+                    streamRequestData.setErrorMessage(parameterSet[35].toString());
+
 
 //                    Map<CategoryCharge, BilledCharge> apiCount = new HashMap<CategoryCharge, BilledCharge>();
 //
@@ -93,16 +95,17 @@ public class BillingStreamProcessor extends StreamProcessor {
 
 
 
-                    BigDecimal hbCommision = new BigDecimal(parameterSet[23].toString());
-                    BigDecimal opCommision = new BigDecimal(parameterSet[21].toString());
-                    BigDecimal spCommision = new BigDecimal(parameterSet[22].toString());
+
 
 
                     BilledCharge billcharge = new BilledCharge(0);
-                    billcharge.setAdscom(hbCommision);
-                    billcharge.setOpcom(opCommision);
-                    billcharge.setSpcom(spCommision);
-                    billcharge.setCount(count);
+                    billcharge.setAdscom(new BigDecimal(parameterSet[32].toString()));
+                    billcharge.setOpcom(new BigDecimal(parameterSet[30].toString()));
+                    billcharge.setSpcom(new BigDecimal(parameterSet[31].toString()));
+                    billcharge.setCount((int) parameterSet[0]);
+
+                    billcharge.setTax( new BigDecimal(parameterSet[33].toString()) );
+
                     billcharge.addTax(BigDecimal.TEN);//todo
 
                     Map<CategoryCharge, BilledCharge> categoryEntry = new HashMap<CategoryCharge, BilledCharge>();
@@ -112,29 +115,42 @@ public class BillingStreamProcessor extends StreamProcessor {
 
                     PriceServiceImpl instance = new PriceServiceImpl();
                     instance.priceNorthBoundRequest(streamRequestData, categoryEntry.entrySet().iterator().next());
-                    //temp code
-
-                    parameterSet[0] = count + 1;
-
-                    double total = (Double) parameterSet[1];
-                    parameterSet[1] = total + 150;
-
-                    double totalOpCommision = (Double) parameterSet[30];
-                    parameterSet[30] = totalOpCommision + 60;
-                    double totalSpCommision = (Double) parameterSet[31];
-                    parameterSet[31] = totalSpCommision + 10;
-                    double totalHbCommision = (Double) parameterSet[32];
-                    parameterSet[32] = totalHbCommision + 30;
-
-
-                    //end of temp code
 
                     parameterSet[20] = "RC-112015";//rate card
                     parameterSet[21] = streamRequestData.getOpcom();  //60;//opCommision
                     parameterSet[22] = streamRequestData.getSpcom(); //10;//spCommision
                     parameterSet[23] = streamRequestData.getAdscom(); //30;//hbCommision
-                    //parameterSet[24] = 15.5;//tax
+                    parameterSet[24] = streamRequestData.getTax() ;//tax
                     parameterSet[25] = streamRequestData.getPrice(); //250;//price
+
+                    parameterSet[30] = billcharge.getOpcom().doubleValue();//totalOpCommision,
+                    parameterSet[31] = billcharge.getSpcom().doubleValue();//totalSpCommision,
+                    parameterSet[32] = billcharge.getAdscom().doubleValue();//totalHbCommision,
+                    parameterSet[33] = billcharge.getTax().doubleValue();//totalTaxAmount,
+
+                    parameterSet[34] = streamRequestData.getStatus();
+                    parameterSet[35] = streamRequestData.getErrorMessage();
+
+                    parameterSet[0] = billcharge.getCount();
+                    parameterSet[1] = billcharge.getPrice().doubleValue();
+                    //temp code
+
+                   // parameterSet[0] = count + 1;
+
+  //                  double total = (Double) parameterSet[1];
+//                    parameterSet[1] = total + 150;
+
+    //                double totalOpCommision = (Double) parameterSet[30];
+      //              parameterSet[30] = totalOpCommision + 60;
+        //            double totalSpCommision = (Double) parameterSet[31];
+          //          parameterSet[31] = totalSpCommision + 10;
+            //        double totalHbCommision = (Double) parameterSet[32];
+              //      parameterSet[32] = totalHbCommision + 30;
+
+
+                    //end of temp code
+
+
 
                 }
 
