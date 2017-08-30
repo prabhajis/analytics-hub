4b8c98a1d26d8a66af7b9939c9c90c9662453a49
dnmadurapperuma <dilan@gmail.com>
2017-08-30 10:57:47 +0530

    killbill sub account

--- components/org.wso2telco.analytics.sparkUdf/src/main/java/org/wso2telco/analytics/sparkUdf/AccountAdder.java
+++ components/org.wso2telco.analytics.sparkUdf/src/main/java/org/wso2telco/analytics/sparkUdf/AccountAdder.java
@@ -4,9 +4,11 @@ import org.killbill.billing.client.KillBillClient;
 import org.killbill.billing.client.KillBillHttpClient;
 import org.killbill.billing.client.model.Account;
 import org.wso2telco.analytics.sparkUdf.configProviders.ConfigurationDataProvider;
+import java.util.UUID;
 
 public class AccountAdder {
 
+	private static ConfigurationDataProvider dataProvider=null;
 	private static KillBillHttpClient killBillHttpClient;
 	private static KillBillClient killBillClient;
 
@@ -38,6 +40,38 @@ public class AccountAdder {
 			account = killBillClient.createAccount(account, user, reason, comment);
 		}catch (Exception e) {
 			return "Did not created";
+		}finally{
+
+			killBillClient.close();
+			killBillHttpClient.close();
+
+		}
+
+		return account.getAccountId().toString();
+	}
+	@SuppressWarnings("deprecation")
+	public String addSubAccount(String perentAcountId, String user,String reason,String comment,String name,String currency,String externalKey,int nameL){
+		Account account=null;
+		try {
+			dataProvider=ConfigurationDataProvider.getInstance();
+
+			killBillHttpClient= new KillBillHttpClient(dataProvider.getUrl(),
+					dataProvider.getUname(),
+					dataProvider.getPassword(),
+					dataProvider.getApiKey(),
+					dataProvider.getApiSecret());
+
+			killBillClient = new KillBillClient(killBillHttpClient);
+			// Create an account
+			account = new Account();
+			account.setName(name);
+			account.setFirstNameLength(nameL);
+			account.setExternalKey(externalKey);
+			account.setCurrency(currency);
+			account.setParentAccountId(UUID.fromString(perentAcountId));
+			account = killBillClient.createAccount(account, user, reason, comment);
+		}catch (Exception e) {
+			return "Child did not created";
 		}finally{
 
 			killBillClient.close();
