4636d85315f5d4f864231c98ab43c5c47eaee42e
dnmadurapperuma <dilan@gmail.com>
2017-08-31 10:37:45 +0530

    change features/org.wso2telco.analytics.killbill.osgi.feature pom

--- components/org.wso2telco.analytics.sparkUdf/src/main/java/org/wso2telco/analytics/sparkUdf/service/BillUpdaterService.java
+++ components/org.wso2telco.analytics.sparkUdf/src/main/java/org/wso2telco/analytics/sparkUdf/service/BillUpdaterService.java
@@ -1,18 +1,19 @@
 package org.wso2telco.analytics.sparkUdf.service;
 
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
 import org.joda.time.LocalDate;
 import org.killbill.billing.client.KillBillClient;
 import org.killbill.billing.client.KillBillClientException;
 import org.killbill.billing.client.KillBillHttpClient;
 import org.killbill.billing.client.RequestOptions;
+import org.killbill.billing.client.model.Account;
 import org.killbill.billing.client.model.Invoice;
 import org.killbill.billing.client.model.InvoiceItem;
 import org.wso2telco.analytics.sparkUdf.configProviders.ConfigurationDataProvider;
-
-import java.math.BigDecimal;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
 
 /**
  * @author dilan
@@ -20,70 +21,73 @@ import java.util.UUID;
 public class BillUpdaterService {
 
 
-    public void updateBillForMonth(String accountId, Date date, String description, int amount) {
-        KillBillHttpClient killBillHttpClient = null;
-        KillBillClient killBillClient = null;
+	private static ConfigurationDataProvider dataProvider=null;
+	private static KillBillHttpClient killBillHttpClient;
+	private static KillBillClient killBillClient;
+	private static UUID invoiceItemId=null;
 
-        try {
-            ConfigurationDataProvider dataProvider = ConfigurationDataProvider.getInstance();
+	@SuppressWarnings("deprecation")
+	public String l(String accountId,int year,int month,String description,int amount){
+		Account account=null;
+		try {
+			dataProvider=ConfigurationDataProvider.getInstance();
 
-            killBillHttpClient = new KillBillHttpClient(dataProvider.getUrl(), dataProvider.getUname(),
-                    dataProvider.getPassword(), dataProvider.getApiKey(), dataProvider.getApiSecret());
+			killBillHttpClient= new KillBillHttpClient(dataProvider.getUrl(),
+					dataProvider.getUname(),
+					dataProvider.getPassword(),
+					dataProvider.getApiKey(),
+					dataProvider.getApiSecret());
 
-            killBillClient = new KillBillClient(killBillHttpClient);
+			killBillClient = new KillBillClient(killBillHttpClient);
 
-            List<Invoice> invoices = killBillClient.getInvoicesForAccount(UUID.fromString(accountId), RequestOptions.empty());
-            Invoice invoice = getInvoiceForMonthFromList(invoices, date);
-            if (invoice != null) {
-                updateInvoice(invoice, description, amount, killBillClient);
-            }
+			List<Invoice> invoices=killBillClient.getInvoicesForAccount(UUID.fromString(accountId));
+			Invoice invoice=getInvoiceForMonthFromList(invoices, year,month);
+			
+			if (invoice!=null) {
+				invoiceItemId=updateInvoice(invoice, description, amount);	
+			}
 
-        } catch (Exception e) {
+		}catch (Exception e) {
+			return "Bill was not updated";
+		}finally{
+			if (killBillClient!=null) {
+				killBillClient.close();
+			}
+			if (killBillHttpClient!=null) {
+				killBillHttpClient.close();
+			}
+		}
+		return invoiceItemId.toString();
+		
+	}
 
-        } finally {
-            if (killBillClient != null) {
-                killBillClient.close();
-            }
-            if (killBillHttpClient != null) {
-                killBillHttpClient.close();
-            }
-        }
-    }
-    /*
-     *
+	private Invoice getInvoiceForMonthFromList(List<Invoice> invoices,int year,int month) {
+		// TODO Auto-generated method stub
+
+		for(Invoice invoice:invoices){
+			LocalDate targetDate=invoice.getTargetDate();
+			if (targetDate.getMonthOfYear()==month && targetDate.getYear()== year ) {
+				return invoice;
+			}
+		}
+		return null; 
+	}
+
+	/*
+	 * 
 	 * To use this method need to use correct killbill client version compatibility metric:http://killbill.io/downloads/
 	 */
 
-    private Invoice getInvoiceForMonthFromList(List<Invoice> invoices, Date date) {
-        // TODO Auto-generated method stub
-
-        for (Invoice invoice : invoices) {
-            LocalDate targetDate = invoice.getTargetDate();
-            if (targetDate.getMonthOfYear() == date.getMonth()) {
-                return invoice;
-            }
-
-        }
-
-        return null;
-    }
-
-    private void updateInvoice(Invoice invoice, String description, int amount, KillBillClient killBillClient) throws
-            KillBillClientException {
-
-        InvoiceItem invoiceItem = createInvoiceItem(invoice, description, amount);
-
-        killBillClient.createExternalCharge(invoiceItem, new LocalDate(System.currentTimeMillis()), true, true, "admin", "usage amount", "usage amount");
-
-    }
-
-    private InvoiceItem createInvoiceItem(Invoice invoice, String description, int amount) {
-        InvoiceItem invoiceItem = new InvoiceItem();
-        invoiceItem.setInvoiceId(invoice.getInvoiceId());
-        invoiceItem.setDescription(description);
-        invoiceItem.setPhaseName(description);
-        invoiceItem.setAmount(BigDecimal.valueOf(new Long(amount)));
-        invoiceItem.setAccountId(invoice.getAccountId());
-        return invoiceItem;
-    }
+	private UUID updateInvoice(Invoice invoice,String description,int amount) throws KillBillClientException {
+		// TODO Auto-generated method stub
+		InvoiceItem invoiceItem=new InvoiceItem();
+		invoiceItem.setInvoiceId(invoice.getInvoiceId());
+		invoiceItem.setDescription(description);
+		invoiceItem.setPhaseName(description);
+		invoiceItem.setAmount(BigDecimal.valueOf(new Long(amount)));
+		invoiceItem.setAccountId(invoice.getAccountId());
+		invoiceItem=killBillClient.createExternalCharge(invoiceItem, new LocalDate(System.currentTimeMillis()),true, true, "admin", "usage amount", "usage amount");
+		return invoiceItem.getInvoiceItemId();
+	}
+	
 }
